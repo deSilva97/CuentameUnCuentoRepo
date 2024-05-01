8e90225986c2f00e644560918632bca70d1f5132
Jorge Garcia Regueiro <jorge.garcia.regueiro@gmail.com>
2024-05-01 12:44:45 +0200

    introducint recover password use case

---
+++ app/src/main/java/es/unir/cuentameuncuento/activities/RecoverPassword.java
@@ -0,0 +1,18 @@
+package es.unir.cuentameuncuento.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.unir.cuentameuncuento.R;
+
+public class RecoverPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_password);
+
+
+    }
+}
