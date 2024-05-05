174ab83f398df637b0737884d5941b5a54e44564
Diego de Silva Setién <dasiga97@gmail.com>
2024-05-05 16:42:35 +0200

    solve credentials point join

--- app/src/main/java/es/unir/cuentameuncuento/helpers/CredentialsHelper.java
+++ app/src/main/java/es/unir/cuentameuncuento/helpers/CredentialsHelper.java
@@ -13,7 +13,7 @@ public class CredentialsHelper {
 
     public static String getEmailRegex(){
         String regex_email = "[a-zA-Z0-9_+&*-]+";
-        String opt_re_email_points = "(?:\\\\.[a-zA-Z0-9_+&*-]+)*";
+        String opt_re_email_points = "(?:\\.[a-zA-Z0-9_+&*-]+)*";
         String regex_prov = "[a-zA-Z0-9]+";
         String regex_domain = "[a-zA-Z]{2,7}";
 
