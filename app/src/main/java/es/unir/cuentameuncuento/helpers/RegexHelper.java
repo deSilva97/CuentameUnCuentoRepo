b5f786e175d09c68cc25c3a478ea9851a2ae4b3b
Diego de Silva Setién <dasiga97@gmail.com>
2024-05-15 14:46:28 +0200

    regex nombre prota

--- app/src/main/java/es/unir/cuentameuncuento/helpers/RegexHelper.java
+++ app/src/main/java/es/unir/cuentameuncuento/helpers/RegexHelper.java
@@ -1,14 +1,17 @@
 package es.unir.cuentameuncuento.helpers;
 
-public class CredentialsHelper {
+public class RegexHelper {
 
     public static boolean verifyEmail(String email){
-
         return  !email.isEmpty() && email.matches(getEmailRegex());
     }
 
     public static boolean verifyPassword(String password){
         return !password.isEmpty() && (password.length() >= 6);
+    }
+
+    public static boolean verifyName(String name){
+        return name.matches(getNameRegex());
     }
 
     public static String getEmailRegex(){
@@ -18,5 +21,12 @@ public class CredentialsHelper {
         String regex_domain = "[a-zA-Z]{2,7}";
 
         return  "^" + regex_email + opt_re_email_points + "@" + regex_prov + "." + regex_domain + "$";
+    }
+
+
+    public static String getNameRegex(){
+        String spanish = "áéíóúÁÉÍÓÚñÑüÜ";
+        String regex = "^[a-zA-Z"+ spanish + "\\s-]{2,}$";
+        return  regex;
     }
 }
